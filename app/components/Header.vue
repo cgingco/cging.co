53393de9e5f44c946a21a816c5a6b052d72c1630
Christian James Gingco <gingcochristian@gmail.com>
2025-11-09 16:53:08 +1100

    Capitalise components

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="absolute top-4 right-4">
     <UColorModeButton
-    class="text-3xl lg:text-4xl"
+    class="text-3xl lg:text-4xl hover:cursor-pointer"
     :class="[
       colorMode.value === 'dark' ? 'text-light bg-dark hover:bg-light hover:text-dark' : 'text-dark bg-light hover:bg-dark hover:text-light'
     ]"/>
@@ -9,7 +9,10 @@
 </template>
 
 <script lang="ts" setup>
-  const colorMode = useColorMode()
+
+const colorMode = useColorMode()
+
+
 </script>
 
 <style>
