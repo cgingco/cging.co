eae25bfe63cdd7fc46aa7edc4fa118a704699736
Christian James Gingco <gingcochristian@gmail.com>
2025-11-09 12:56:01 +1100

    Add @nuxt/ui

--- app/app.vue
+++ app/app.vue
@@ -1,18 +1,7 @@
 <template>
-  <div style="
-	height: 100vh;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-	font-family: Helvetica, Arial, sans-serif;
-	line-height: 0;
-  ">
-	<p>Hello, I'm Christian Gingco.</p>
-	<p>This website is still under construction, check back soon!</p>
-	<p>In the meantime, you can find me on <a href="https://www.linkedin.com/in/christian-gingco/" target="_blank" rel="noopener noreferrer">LinkedIn</a> or <a href="https://github.com/cgingco" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
-  </div>
+  <UApp>
+    <NuxtPage />
+  </UApp>
 </template>
 
 <script setup lang="ts">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxthub/core',
     '@nuxtjs/color-mode',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
+    '@nuxt/ui',
     '@nuxt/content'
   ],
   devtools: { enabled: true }
